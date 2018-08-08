b4d1bed9f2d56cd72defe54909a8f7618e87d3b3
lstern22 <lstern220@gmail.com>
2018-08-08 18:27:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code in this file!
 function scuberGreetingForFeet(ride){
   if(ride <= 400){
-    console.log ("free sample");
+    console.log ("This one is on me!");
   }
     
 }else if (){
